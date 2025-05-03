5e38fa7c99a990eae7f4a62f98c2a1e19c446c7f
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2025-05-04 01:03:54 +0530

    add

--- app/server.js
+++ app/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Node.js API is running inside a Docker container.');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World! Node.js API is running inside a Docker container.');
